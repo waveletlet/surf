8d293caa2005747ff992d7de2f7f0cc3e6dbc8c0
x <billmurray@tutanota.de>
2017-09-27 20:43:51 +0200

    Update surf.go

--- surf.go
+++ surf.go
@@ -2,9 +2,9 @@
 package surf
 
 import (
-	"github.com/headzoo/surf/agent"
-	"github.com/headzoo/surf/browser"
-	"github.com/headzoo/surf/jar"
+	"github.com/lostinblue/surf/agent"
+	"github.com/lostinblue/surf/browser"
+	"github.com/lostinblue/surf/jar"
 )
 
 var (
