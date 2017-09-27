cb7949d83d9f17303ab6d8670c6ac5c7bae7460b
x <billmurray@tutanota.de>
2017-09-27 20:47:23 +0200

    Update bookmarks.go

--- jar/bookmarks.go
+++ jar/bookmarks.go
@@ -2,8 +2,8 @@ package jar
 
 import (
 	"encoding/json"
-	"github.com/headzoo/surf/errors"
-	"github.com/headzoo/surf/util"
+	"github.com/lostinblue/surf/errors"
+	"github.com/lostinblue/surf/util"
 	"io/ioutil"
 	"os"
 )
