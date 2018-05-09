58b9cc1db3564db199baedf1af1998731f564c0d
Eric Zis <eziscky@gmail.com>
2018-05-09 20:50:00 +0300

    added gzip and deflate response support

--- browser/browser.go
+++ browser/browser.go
@@ -2,6 +2,8 @@ package browser
 
 import (
 	"bytes"
+	"compress/flate"
+	"compress/gzip"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -847,7 +849,21 @@ func (bow *Browser) httpRequest(req *http.Request) error {
 	if resp.Body != nil {
 		defer resp.Body.Close()
 
-		bow.body, err = ioutil.ReadAll(resp.Body)
+		var reader io.Reader
+		switch resp.Header.Get("Content-Encoding") {
+		case "gzip":
+			reader, err = gzip.NewReader(resp.Body)
+			if err != nil {
+				return err
+			}
+		case "deflate":
+			reader = flate.NewReader(resp.Body)
+
+		default:
+			reader = resp.Body
+		}
+
+		bow.body, err = ioutil.ReadAll(reader)
 		if err != nil {
 			return err
 		}
